1fa6471050ce66f8e822973b04ebeef2c0c14adb
marsianss <wassimbachtour525@gmail.com>
2024-09-20 13:03:32 +0200

    Laravel video 5

--- resources/views/Components/nav-link.blade.php
+++ resources/views/Components/nav-link.blade.php
@@ -1,5 +1,7 @@
-@props(['active']) 
-<a class="{{ request()->is('/') ? 'bg-gray-900 text-white': 'text-gray-300 hover:bg-gray-700 hover:text-white' }} rounded-md px-3 py-2 text-sm font-medium"
- aria-current="{{ request()->is('/') ? 'page': 'false' }}"
+@props(['active' => false])
+
+
+<a class="{{ $active ? 'bg-gray-900 text-white': 'text-gray-300 hover:bg-gray-700 hover:text-white' }} rounded-md px-3 py-2 text-sm font-medium"
+ aria-current="{{ $active ? 'page': 'false' }}"
  {{ $attributes }}
 >{{ $slot }}</a>
